3c3df7cc2969d048550d4793a87e648e32983908
Roman Sokolov <gnonasis@gmail.com>
2025-04-25 18:10:14 +0300

    add CORSMiddleware

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes import (
     movie_router,
@@ -10,6 +11,19 @@ app = FastAPI(
     title="Movies homework",
     description="Description of project"
 )
+
+origins = [
+    "http://localhost:8025",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 api_version_prefix = "/api/v1"
 
