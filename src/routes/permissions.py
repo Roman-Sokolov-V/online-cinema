e7ec9706e7b4523d8d54008181bac9bcfd385268
Roman Sokolov <gnonasis@gmail.com>
2025-05-02 18:45:33 +0300

    add is_moderator_or_admin permission

--- src/routes/permissions.py
+++ src/routes/permissions.py
@@ -53,3 +53,18 @@ def is_owner_or_admin(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="You must be owner or admin to update profile"
         )
+
+
+def is_moderator_or_admin(
+        payload: AccessTokenPayload = Depends(get_access_token_payload)
+) -> AccessTokenPayload:
+    """
+    In addition to catalog and user interface access, can manage movies
+    on the site through the admin panel, view sales, etc.
+    """
+    if payload["group"] not in ["admin", "moderator"]:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You are not admin."
+        )
+    return payload
