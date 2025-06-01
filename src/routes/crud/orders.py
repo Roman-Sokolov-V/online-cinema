0701c4cd23445f1e82c7724512ea519f43859f3a
Roman Sokolov <gnonasis@gmail.com>
2025-06-01 18:15:54 +0300

    improved set_status_canceled

--- src/routes/crud/orders.py
+++ src/routes/crud/orders.py
@@ -30,14 +30,49 @@ def get_orders_stmt(
         .selectinload(OrderItemModel.movie)
     )
 
-async def set_status_canceled(session_id: str, db: AsyncSession) -> None:
-    stmt = select(OrderModel).where(OrderModel.session_id == session_id)
-    result = await db.execute(stmt)
-    order: OrderModel | None = result.scalars().one_or_none()
+async def set_status_canceled(
+        db: AsyncSession,
+        session_id: str | None = None,
+        order: OrderModel | None = None,
+) -> None:
+    """
+    Set the status of an order to CANCELED.
+
+    This function cancels an order by setting its status to CANCELED
+    if it is not already paid or canceled. You can either provide the order
+    directly or specify the session_id to look up the order in the database.
+
+    Args:
+        db (AsyncSession): The asynchronous database session.
+        session_id (str | None): The Stripe session ID used to find the order,
+            if the order is not provided directly. Used when canceling an order
+            through a Stripe webhook.
+        order (OrderModel | None): An optional order instance used when
+            canceling an order manually. If not provided, the order will be
+            fetched using the session_id.
+
+    Raises:
+        HTTPException:
+            - 404 if the order is not found (when using session_id).
+            - 400 if the order is already paid.
+            - 409 if the order is already canceled.
+    """
     if order is None:
+        stmt = select(OrderModel).where(OrderModel.session_id == session_id)
+        result = await db.execute(stmt)
+        order: OrderModel | None = result.scalars().one_or_none()
+        if order is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Order not found in your orders",
+            )
+    if order.status == OrderStatus.PAID:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Order not found"
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Order already paid"
+        )
+    if order.status == OrderStatus.CANCELED:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail="Order already cancelled"
         )
     order.status = OrderStatus.CANCELED
     await db.commit()
