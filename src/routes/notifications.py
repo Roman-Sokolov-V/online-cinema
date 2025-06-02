cb806de35e734b462b2f0efc9732d88fc9f0aaff
Roman Sokolov <gnonasis@gmail.com>
2025-06-02 09:48:53 +0300

    add documentation to notification endpoints

--- src/routes/notifications.py
+++ src/routes/notifications.py
@@ -3,7 +3,6 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 
-
 router = APIRouter()
 
 
@@ -11,15 +10,23 @@ templates = Jinja2Templates(directory="notifications/templates")
 
 @router.get(
     "/success/",
+    summary="Successful payment",
+    description="Shows html page with successful payment notification",
     response_class=HTMLResponse
 )
 async def paid_success(request: Request):
-    return templates.TemplateResponse("success_paid.html", {"request": request})
+    return templates.TemplateResponse(
+        "success_paid.html", {"request": request}
+    )
 
 
 @router.get(
     "/cancel/",
+    summary="Canceled payment",
+    description="Shows html page with notification about canceled payment",
     response_class=HTMLResponse
 )
 async def paid_canceled(request: Request):
-    return templates.TemplateResponse("canceled_paid.html", {"request": request})
+    return templates.TemplateResponse(
+        "canceled_paid.html", {"request": request}
+    )
